0aa60805236abce433be324e759a353f8e4461cd
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2018-01-16 17:47:58 +0900

    gsstep for matrix

--- include/gsstep.hpp
+++ include/gsstep.hpp
@@ -25,29 +25,56 @@ namespace marlib {
     dense_matrix<ValueT,RangeT> A,
     const ValueT& sigma,
     const ValueT& omega,
-    vector<ValueT,RangeT> b, vector<ValueT,RangeT> x);
+    dense_matrix<ValueT,RangeT> b, dense_matrix<ValueT,RangeT> x);
 
   template <typename ValueT, typename RangeT>
   void gsstep_impl(const trans_t& trans, const ValueT& alpha,
     const csr_matrix<ValueT, RangeT>& A,
     const ValueT& sigma,
     const ValueT& omega,
-    vector<ValueT,RangeT> b, vector<ValueT,RangeT> x);
+    dense_matrix<ValueT,RangeT> b, dense_matrix<ValueT,RangeT> x);
+
+  // dense
 
   template <typename ValueT, typename RangeT>
   vector<ValueT,RangeT>& gsstep(const trans_t& trans, const ValueT& alpha,
     const dense_matrix<ValueT,RangeT>& A, const ValueT& sigma, const ValueT& omega,
     const vector<ValueT,RangeT>& b, vector<ValueT,RangeT>& x) {
+
+    dense_matrix<ValueT,RangeT> bmat(b.size(), 1, const_cast<ValueT*>(b.ptr()), b.size());
+    dense_matrix<ValueT,RangeT> xmat(x.size(), 1, x.ptr(), x.size());
+
+    gsstep_impl(trans, alpha, A, sigma, omega, bmat, xmat);
+    return x;
+  }
+
+  template <typename ValueT, typename RangeT>
+  dense_matrix<ValueT,RangeT>& gsstep(const trans_t& trans, const ValueT& alpha,
+    const dense_matrix<ValueT,RangeT>& A, const ValueT& sigma, const ValueT& omega,
+    const dense_matrix<ValueT,RangeT>& b, dense_matrix<ValueT,RangeT>& x) {
     gsstep_impl(trans, alpha, A, sigma, omega, b, x);
     return x;
   }
+
+  /// csr
 
   template <typename ValueT, typename RangeT>
   vector<ValueT,RangeT>& gsstep(const trans_t& trans, const ValueT& alpha,
     const csr_matrix<ValueT,RangeT>& A, const ValueT& sigma, const ValueT& omega,
     const vector<ValueT,RangeT>& b, vector<ValueT,RangeT>& x) {
-    gsstep_impl(trans, alpha, A, sigma, omega, b, x);
+
+    dense_matrix<ValueT,RangeT> bmat(b.size(), 1, const_cast<ValueT*>(b.ptr()), b.size());
+    dense_matrix<ValueT,RangeT> xmat(x.size(), 1, x.ptr(), x.size());
+
+    gsstep_impl(trans, alpha, A, sigma, omega, bmat, xmat);
     return x;
   }
 
+  template <typename ValueT, typename RangeT>
+  dense_matrix<ValueT,RangeT>& gsstep(const trans_t& trans, const ValueT& alpha,
+    const csr_matrix<ValueT,RangeT>& A, const ValueT& sigma, const ValueT& omega,
+    const dense_matrix<ValueT,RangeT>& b, dense_matrix<ValueT,RangeT>& x) {
+    gsstep_impl(trans, alpha, A, sigma, omega, b, x);
+    return x;
+  }
 }
--- src/gsstep.cpp
+++ src/gsstep.cpp
@@ -35,42 +35,63 @@ namespace marlib {
     dense_matrix<ValueT,RangeT> A,
     const ValueT& sigma,
     const ValueT& omega,
-    vector<ValueT,RangeT> b, vector<ValueT,RangeT> x) {
+    dense_matrix<ValueT,RangeT> b, dense_matrix<ValueT,RangeT> x) {
     assert(A.nrow() == A.ncol());
+    assert(b.ncol() == x.ncol());
     size_type n = A.nrow();
+    size_type nrhs = b.ncol();
     A.set_range(range<RangeT>(1,n),range<RangeT>(1,n));
-    b.set_range(range<RangeT>(1,n));
-    x.set_range(range<RangeT>(1,n));
+    b.set_range(range<RangeT>(1,n),range<RangeT>(1,nrhs));
+    x.set_range(range<RangeT>(1,n),range<RangeT>(1,nrhs));
+
+    vector<ValueT,RangeT> tmpx(nrhs);
+    tmpx.set_range(range<RangeT>(1,nrhs));
 
     switch (trans) {
       case NoTrans:
         for (RangeT i=1; i<=n; i++) {
           ValueT tmpd = 0;
-          ValueT tmpx = b(i) / alpha;
+          for (RangeT u=1; u<=nrhs; u++) {
+            tmpx(u) = b(i,u) / alpha;
+          }
           for (RangeT j=1; j<=n; j++) {
             if (i == j) {
               tmpd = A(i,j);
-              tmpx += sigma * x(j);
+              for (RangeT u=1; u<=nrhs; u++) {
+                tmpx(u) += sigma * x(j,u);
+              }
             } else {
-              tmpx -= A(i,j) * x(j);
+              for (RangeT u=1; u<=nrhs; u++) {
+                tmpx(u) -= A(i,j) * x(j,u);
+              }
             }
           }
-          x(i) = omega * tmpx / tmpd + (1 - omega) * x(i);
+          for (RangeT u=1; u<=nrhs; u++) {
+            x(i,u) = omega * tmpx(u) / tmpd + (1 - omega) * x(i,u);
+          }
         }
         break;
       case Trans:
         for (RangeT j=1; j<=n; j++) {
           ValueT tmpd = 0;
-          ValueT tmpx = b(j) / alpha;
+          for (RangeT u=1; u<=nrhs; u++) {
+            tmpx(u) = b(j,u) / alpha;
+          }
           for (RangeT i=1; i<=n; i++) {
             if (i == j) {
               tmpd = A(i,j);
-              tmpx += sigma * x(i);
+              for (RangeT u=1; u<=nrhs; u++) {
+                tmpx(u) += sigma * x(i,u);
+              }
             } else {
-              tmpx -= A(i,j) * x(i);
+              for (RangeT u=1; u<=nrhs; u++) {
+                tmpx(u) -= A(i,j) * x(i,u);
+              }
             }
           }
-          x(j) = omega * tmpx / tmpd + (1 - omega) * x(j);
+          for (RangeT u=1; u<=nrhs; u++) {
+            x(j,u) = omega * tmpx(u) / tmpd + (1 - omega) * x(j,u);
+          }
         }
         break;
     }
@@ -81,33 +102,46 @@ namespace marlib {
     const csr_matrix<ValueT, RangeT>& A,
     const ValueT& sigma,
     const ValueT& omega,
-    vector<ValueT,RangeT> b, vector<ValueT,RangeT> x) {
+    dense_matrix<ValueT,RangeT> b, dense_matrix<ValueT,RangeT> x) {
     assert(A.nrow() == A.ncol());
+    assert(b.ncol() == x.ncol());
     size_type n = A.nrow();
-    b.set_range(range<RangeT>(0,n-1));
-    x.set_range(range<RangeT>(0,n-1));
+    size_type nrhs = b.ncol();
+    b.set_range(range<RangeT>(0,n-1),range<RangeT>(1,nrhs));
+    x.set_range(range<RangeT>(0,n-1),range<RangeT>(1,nrhs));
+
+    vector<ValueT,RangeT> tmpx(nrhs);
+    tmpx.set_range(range<RangeT>(1,nrhs));
 
     switch (trans) {
       case NoTrans:
         for (size_type i=0; i<A.nrow(); i++) {
           ValueT tmpd = 0;
-          ValueT tmpx = b(i) / alpha;
+          for (RangeT u=1; u<=nrhs; u++) {
+            tmpx(u) = b(i,u) / alpha;
+          }
           for (size_type z=A.rowptr(i); z<A.rowptr(i+1); z++) {
             size_type j = A.colind(z);
             if (i == j) {
               tmpd = A.value(z);
-              tmpx += sigma * x(i);
+              for (RangeT u=1; u<=nrhs; u++) {
+                tmpx(u) += sigma * x(i,u);
+              }
             } else {
-              tmpx -= A.value(z) * x(j);
+              for (RangeT u=1; u<=nrhs; u++) {
+                tmpx(u) -= A.value(z) * x(j,u);
+              }
             }
           }
-          x(i) = omega * tmpx / tmpd + (1 - omega) * x(i);
+          for (RangeT u=1; u<=nrhs; u++) {
+            x(i,u) = omega * tmpx(u) / tmpd + (1 - omega) * x(i,u);
+          }
         }
         break;
       case Trans:
-        vector<ValueT,RangeT> tmp(n);
+        dense_matrix<ValueT,RangeT> tmp(n,nrhs);
         vector<size_type,RangeT> diag(n);
-        tmp.set_range(range<RangeT>(0,n-1));
+        tmp.set_range(range<RangeT>(0,n-1),range<RangeT>(1,nrhs));
         diag.set_range(range<RangeT>(0,n-1));
         tmp = 0;
         daxpy(1/alpha, b, tmp);
@@ -116,18 +150,26 @@ namespace marlib {
             size_type j = A.colind(z);
             if (i == j) {
               diag(i) = z;
-              tmp(i) += sigma * x(i);
+              for (RangeT u=1; u<=nrhs; u++) {
+                tmp(i,u) += sigma * x(i,u);
+              }
               break;
             } else {
-              tmp(j) -= A.value(z) * x(i);
+              for (RangeT u=1; u<=nrhs; u++) {
+                tmp(j,u) -= A.value(z) * x(i,u);
+              }
             }
           }
         }
         for (size_type i=0; i<n; i++) {
-          x(i) = omega * tmp(i) / A.value(diag(i)) + (1 - omega) * x(i);
+          for (RangeT u=1; u<=nrhs; u++) {
+            x(i,u) = omega * tmp(i,u) / A.value(diag(i)) + (1 - omega) * x(i,u);
+          }
           for (size_type z=diag(i)+1; z<A.rowptr(i+1); z++) {
             size_type j = A.colind(z);
-            tmp(j) -= A.value(z) * x(i);
+            for (RangeT u=1; u<=nrhs; u++) {
+              tmp(j,u) -= A.value(z) * x(i,u);
+            }
           }
         }
         break;
@@ -137,10 +179,10 @@ namespace marlib {
   template void gsstep_impl(const trans_t& trans, const double& alpha,
     dense_matrix<double,int> A,
     const double& sigma, const double& omega,
-    vector<double,int> b, vector<double,int> x);
+    dense_matrix<double,int> b, dense_matrix<double,int> x);
 
   template void gsstep_impl(const trans_t& trans, const double& alpha,
     const csr_matrix<double,int>& A,
     const double& sigma, const double& omega,
-    vector<double,int> b, vector<double,int> x);
+    dense_matrix<double,int> b, dense_matrix<double,int> x);
 }
